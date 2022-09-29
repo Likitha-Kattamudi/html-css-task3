3aca1587d4e459b08b12ede6708371cc0b1f17e6
Likitha-Kattamudi <113127686+Likitha-Kattamudi@users.noreply.github.com>
2022-09-29 18:35:05 +0530

    Add files via upload
    This is JavaScript file for task3-Contact page.

--- task3-ContactPageJavaScript.js
+++ task3-ContactPageJavaScript.js
@@ -7,15 +7,36 @@
             var selectedOption=myform.select[myform.select.selectedIndex].text
             var addPromoCode="-PROMO";
             addPromoCode=selectedOption.concat(addPromoCode);
-        document.getElementById("promo").value=addPromoCode;
+        document.getElementById("myPromoCode").value=addPromoCode;
         
            
          }
         function validate(){
+      
         
             var name=document.getElementById("myName").value;
             var email=document.getElementById("myEmail").value;
             var organization=document.getElementById("myOrganization").value;
+            
+            var telephoneNo=document.getElementById("myTelephoneNo").value;
+            
+            var city=document.getElementById("myCity").value;
+            
+            var promoCode=document.getElementById("myPromoCode").value;
+            
+            var webSiteAddress=document.getElementById("myWebSiteAddress").value;
+            
+            var myLikings=document.getElementById("myLikings").value;
+            
+            var aboutOrganisation=document.getElementById("aboutOrganisation").value;
+            var female=document.getElementById("female");
+            var male=document.getElementById("male");
+            var modeIsEmail=document.getElementById("modeIsEmail");
+      
+            var phone=document.getElementById("phone");
+            var both=document.getElementById("both");
+            var state=myform.select[myform.select.selectedIndex].text;
+            
             
             if (name==""){
              
@@ -23,6 +44,9 @@
                document.getElementById("labelText1").innerHTML="Name is Required";
              document.getElementById("labelText1").style.visibility="visible";
              document.getElementById("labelText1").style.color="red"; 
+            }
+            if(organization==""){
+               document.getElementById("validationMessage").innerHTML="Please fill all the required fields below";
             }
             
             if(email==""){
@@ -40,28 +64,87 @@
              document.getElementById("labelText2").style.visibility="visible";
              document.getElementById("labelText2").style.color="red";
             }
+
+            }
+            if(female.checked==true){
+               localStorage.setItem('gender',female.value);
+            }
+            if(male.checked==true){
+               localStorage.setItem('gender',male.value);
+            }
+            if(modeIsEmail.checked==true){
+               localStorage.setItem('mode',modeIsEmail.value);
+            }
+            if(phone.checked==true){
+               localStorage.setItem('mode',phone.value);
+            }
+            if(both.checked==true){
+               localStorage.setItem('mode',both.value);
+            }
+         
+            localStorage.setItem('name',name);
+            localStorage.setItem('email',email);
+            localStorage.setItem('organization',organization);
+            localStorage.setItem('telephoneNo',telephoneNo);
+            localStorage.setItem('city',city);
+            localStorage.setItem('state',state);
+            localStorage.setItem('promoCode',promoCode);
+            localStorage.setItem('webSiteAddress',webSiteAddress);
+            localStorage.setItem('aboutOrganisation',aboutOrganisation);
+            localStorage.setItem('myLikings',myLikings);
          }
-         if(organization==""){
-            document.getElementById("validationMessage").innerHTML="Please fill all the required fields below";
-         }
-        }
+         
+        
     
         
          function genderChange(value){
            var gender=value;
            
            
-           if (gender=="Female"){
+           if (gender=="female"){
             alert("Hello Lady")
-        
-            
            }
            else{
         
-            myAlert("Hello Sir");
+            alert("Hello Sir");
             
            }
-
+         }
+         function loadData(){
+             {
+                 document.getElementById("myName").value=localStorage.getItem('name');
+                 document.getElementById("myEmail").value=localStorage.getItem('email');
+                 document.getElementById("myOrganization").value=localStorage.getItem('organization');
+                 document.getElementById("myTelephoneNo").value=localStorage.getItem('telephoneNo');
+                 document.getElementById("myCity").value=localStorage.getItem('city');
+                 document.getElementById("myPromoCode").value=localStorage.getItem('promoCode');
+                 document.getElementById("myWebSiteAddress").value=localStorage.getItem('webSiteAddress');
+                 document.getElementById("myLikings").value=localStorage.getItem('myLikings');
+                 document.getElementById("aboutOrganisation").value=localStorage.getItem('aboutOrganisation');
+                 //document.getElementById("myGender").value=localStorage.getItem('gender');
+                 document.getElementById("myState").value=localStorage.getItem('state');
+                 if(localStorage.getItem('mode')==modeIsEmail.value){
+                  variableToCheck=document.getElementById("modeIsEmail");
+                  variableToCheck.checked=true;
+                 }
+                 if(localStorage.getItem('mode')==phone.value){
+                  variableToCheck=document.getElementById("phone");
+                  variableToCheck.checked=true;
+                 }
+                 if(localStorage.getItem('mode')==both.value){
+                  variableToCheck=document.getElementById("both");
+                  variableToCheck.checked=true;
+                 }
+                 if(localStorage.getItem('gender')==female.value){
+                  variableToCheck=document.getElementById("female");
+                  variableToCheck.checked=true;
+                 }
+                 if(localStorage.getItem('gender')==male.value){
+                  variableToCheck=document.getElementById("male");
+                  variableToCheck.checked=true;
+                 }
+                 
+             }
          }
         
          
